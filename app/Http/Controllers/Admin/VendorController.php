fa3cef73b6d1193e83c7ac21bc6849daf93865c0
mhmedrady <mhmed.rady.95@gmail.com>
2021-03-09 20:02:25 +0200

    lar.com update

--- app/Http/Controllers/Admin/VendorController.php
+++ app/Http/Controllers/Admin/VendorController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class vendorController extends Controller
+class VendorController extends Controller
 {
     public function getAll(){
         return view("admin.pages.Vendors");
--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -9,5 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class Vendor extends Model
 {
     use HasFactory,Notifiable;
-    protected $fillable = ["name","logo","mobile","address","email","active","Category_id","created_at","updated_at"];
+    protected $fillable = ["name","logo","mobile","address","email","active","created_at","updated_at"];
+
+    protected $hidden = ["Category_id"];
 }
